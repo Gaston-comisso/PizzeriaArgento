4c3aa9d657f8753ddafc44f931ddccebb3adc357
franco castillo <francoantonioredondo@gmail.com>
2024-05-26 12:18:05 -0300

    se creo el archivo .env para la bdd, se agrego configuracion para la bdd, se agrego ruta para insertar formularios

--- PIZZERIA-ARGENTA/back.py
+++ PIZZERIA-ARGENTA/back.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL, MySQLdb
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
+# Cargar variables de entorno desde un archivo .env
+load_dotenv()
+
+#Conexion a BDD
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') 
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') 
+mysql = MySQL(app)
+
+
+#rutas
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -23,6 +37,28 @@ def pedidos():
 def formulario():
     return render_template("formulario.html")
 
+
+#ruta para recibir el formulario
+@app.route('/cargarFormulario', methods=['POST'])
+def cargarFormulario(): #query
+    if  request.method == 'POST':
+        telefono = request.form['telefono']
+        nombre = request.form['nombre']
+        email= int(request.form['email'])
+        barrio= int(request.form['barrio'])
+        direccion= int(request.form['direccion'])
+        costo= int(request.form['costo'])
+        comentario= int(request.form['comentario'])      
+        cur = mysql.connection.cursor()    
+        
+        cur.execute('''INSERT INTO formularios (telefono, nombre, email, barrio, direccion,costo,comentario)
+                    VALUES (%s, %s, %s, %s,%s,%s,%s)'''
+                    , (telefono,nombre,email,barrio,direccion,costo,comentario))
+        
+        mysql.connection.commit()
+        
+        
+        return render_template('admin.html') #faltaria agregar bien el template al que se debe redirigir
 
 if __name__=="__main__":
     app.run(debug=True)
